902a368c65e35971d40f7408204b23921c5a6d09
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-02-21 21:00:04 +0000

    hardcoded accession numbers for the zhao data

---
+++ cd4_dataset.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# SRA000206
+METHYLATIONS = [
+    #dict(experiment_accession='SRX000138', experiment_alias='CTCF', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000139', experiment_alias='H2A.Z', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000140', experiment_alias='H2BK5me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000141', experiment_alias='H3K27me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000142', experiment_alias='H3K27me2', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000143', experiment_alias='H3K27me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000144', experiment_alias='H3K36me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000145', experiment_alias='H3K36me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000146', experiment_alias='H3K4me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000147', experiment_alias='H3K4me2', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000148', experiment_alias='H3K4me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000149', experiment_alias='H3K79me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000150', experiment_alias='H3K79me2', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000151', experiment_alias='H3K79me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000152', experiment_alias='H3K9me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000153', experiment_alias='H3K9me2', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000154', experiment_alias='H3K9me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000155', experiment_alias='H3R2me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000156', experiment_alias='H3R2me2', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000157', experiment_alias='H4K20me1', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000158', experiment_alias='H4K20me3', study_accession='SRP000201'),
+    dict(experiment_accession='SRX000159', experiment_alias='H4R3me2', study_accession='SRP000201'), # Also H2A
+    #dict(experiment_accession='SRX000160', experiment_alias='Pol II', study_accession='SRP000201'),
+]
+
+# SRA000287
+ACETYLATIONS = [
+    dict(experiment_accession='SRX000354', experiment_alias='H2AK5ac', study_accession='SRP000200'), 
+    dict(experiment_accession='SRX000355', experiment_alias='H2AK9ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000356', experiment_alias='H2BK120ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000357', experiment_alias='H2BK12ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000358', experiment_alias='H2BK20ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000359', experiment_alias='H2BK5ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000360', experiment_alias='H3K14ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000361', experiment_alias='H3K18ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000362', experiment_alias='H3K23ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000363', experiment_alias='H3K27ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000364', experiment_alias='H3K27me3', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000365', experiment_alias='H3K36ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000366', experiment_alias='H3K4ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000367', experiment_alias='H3K79me1', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000368', experiment_alias='H3K79me2', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000369', experiment_alias='H3K79me3', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000370', experiment_alias='H3K9ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000371', experiment_alias='H4K12ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000372', experiment_alias='H4K16ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000373', experiment_alias='H4K5ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000374', experiment_alias='H4K8ac', study_accession='SRP000200'),
+    dict(experiment_accession='SRX000375', experiment_alias='H4K91ac', study_accession='SRP000200'),
+
+]
