6c4fc1aad09248f7d98a717d348d6ba10831c5fc
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-02-23 20:55:14 +0000

    added deadzones task

--- command_line_applications/rseg.py
+++ command_line_applications/rseg.py
@@ -2,3 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+try:
+    from sh import deadzones
+except ImportError:
+    raise ImportError('Cannot import rseg or one of its utilities, ensure it is correctly installed')
--- peak_calling/rseg.py
+++ peak_calling/rseg.py
@@ -2,11 +2,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+import os
+import shutil
 from genome_browser import GenomeSequence
 from task import Task, luigi, GzipOutputFile
 
 import requests
 import logging
+from util import temporary_directory
+
 
 class UnfinishedGenomeSections(Task):
 
@@ -66,27 +70,53 @@ class Deadzones(Task):
     genome_version = luigi.Parameter()
     width_of_kmers = luigi.IntParameter()
 
-    prefix_length = luigi.IntParameter(default=15, significant=False)
+    prefix_length = luigi.IntParameter(default=3, significant=False)
+    verbose = luigi.BooleanParameter(default=True, significant=False)
 
     @property
     def _genome_sequence_task(self):
         return GenomeSequence(genome_version=self.genome_version)
 
-    @property
-    def _unfinished_sections_task(self):
-        return UnfinishedGenomeSections(genome_version=self.genome_version)
-
     def requires(self):
-        return [self._genome_sequence_task, self._unfinished_sections_task]
+        return [self._genome_sequence_task]
 
     def parameters(self):
         return [self.genome_version, 'k{}'.format(self.width_of_kmers)]
 
+    @property
+    def _extension(self):
+        return 'bed'
+
     def run(self):
-        pass
+        from command_line_applications.rseg import deadzones
+        from command_line_applications.ucsc_suite import twoBitToFa
+
+        logger = logging.getLogger('Deadzones')
+        output_abspath = os.abspath(self.output().path)
+        genome_sequence_abspath = os.abspath(self._genome_sequence_task.output().path)
+        with temporary_directory(prefix='tmp-deadzones-',
+                                 cleanup_on_exception=False, logger=logger):
+            fa_sequence_filename = 'sequence.fa'
+            logger.debug('Converting sequence to fasta')
+            twoBitToFa(genome_sequence_abspath, fa_sequence_filename)
+
+            deadzones_output_file = 'deadzones.bed'
+
+            deadzones_args = ['-p', self.prefix_length,
+                              '-s', 'fa',
+                              '-k', self.width_of_kmers,
+                              '-o', deadzones_output_file,
+                              ]
+            if self.verbose:
+                deadzones_args.append('-v')
+            logger.debug('Computing deadzones (takes ages :( )')
+            deadzones(*deadzones_args)
+            logger.debug('Deadzones done, moving file to output location')
+            shutil.move(deadzones_output_file, output_abspath)
 
 
 if __name__ == '__main__':
     logging.getLogger('UnfinishedGenomeSections').setLevel(logging.DEBUG)
+    logging.getLogger('Deadzones').setLevel(logging.DEBUG)
     logging.basicConfig()
     luigi.run()
