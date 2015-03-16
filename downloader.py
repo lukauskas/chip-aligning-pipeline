3c1e5b104ae3584eb64b0c8bac20bbb46af12917
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-03-16 15:42:49 +0000

    added md5 checksum verification

--- downloader.py
+++ downloader.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 import ftplib
 import os
+import hashlib
 import requests
 import tempfile
 import shutil
@@ -32,7 +33,17 @@ def _fetch_from_http(url, output):
             output.write(chunk)
 
 
-def fetch(url, output):
+def md5_hash(buffer, blocksize=2**20):
+    # Based on http://stackoverflow.com/a/4213255
+    hash = hashlib.md5()
+    for chunk in iter(lambda: buffer.read(blocksize), b''):
+        hash.update(chunk)
+    return hash.hexdigest()
+
+class ChecksumMismatch(Exception):
+    pass
+
+def fetch(url, output, md5_checksum=None):
     parsed_url = urlparse(url)
 
     if parsed_url.scheme in ['http', 'https']:
@@ -41,3 +52,11 @@ def fetch(url, output):
         _fetch_from_ftp(url, output)
     else:
         raise ValueError("unsupported url scheme {0!r}".format(parsed_url.scheme))
+
+    if md5_checksum is not None:
+        output.seek(0)
+        actual_checksum = md5_hash(output)
+
+        if actual_checksum != md5_checksum:
+            raise ChecksumMismatch('Checksums don\'t match for {}. Got {} != {} (expected)'.format(url, actual_checksum,
+                                                                                                   md5_checksum))
--- ena_downloader.py
+++ ena_downloader.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import logging
+import hashlib
 import requests
 from task import Task
 import luigi
@@ -12,10 +13,11 @@ import os
 import shutil
 from downloader import fetch
 
-_EXPERIMENT_URI_FORMAT = 'https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession={accession}&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,experiment_alias,run_accession,tax_id,scientific_name,instrument_model,library_layout,fastq_ftp'
+_EXPERIMENT_URI_FORMAT = 'https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession={accession}&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,experiment_alias,run_accession,tax_id,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_md5'
 
-def _fetch_run_data_for_experiment(experiment_accession):
-    logger = logging.getLogger('ena_downloader.fetch_runs')
+def _fetch_run_data_for_experiment(experiment_accession, logger=None):
+    if logger is None:
+        logger = logging.getLogger('ena_downloader.fetch_runs')
     uri = _EXPERIMENT_URI_FORMAT.format(accession=experiment_accession)
     logger.debug('Fetching {}'.format(uri))
 
@@ -55,12 +57,12 @@ class ShortReadsForExperiment(Task):
         return 'tar.bz2'
 
     def run(self):
-        logger = ShortReadsForExperiment.logger()
+        logger = self.logger()
 
         from command_line_applications.archiving import tar
 
         logger.debug('Fetching runs for {}-{}'.format(self.study_accession, self.experiment_accession))
-        runs = _fetch_run_data_for_experiment(self.experiment_accession)
+        runs = _fetch_run_data_for_experiment(self.experiment_accession, logger=logger)
         logger.debug('Got {} runs'.format(len(runs)))
 
         for run in runs:
@@ -89,8 +91,8 @@ class ShortReadsForExperiment(Task):
 
                 run_url = 'ftp://' + run['fastq_ftp']
                 logger.debug('Fetching: {} to {}'.format(run_url, run_sequence_filename))
-                with open(run_sequence_filename, 'w') as run_sequence_filehandle:
-                    fetch(run_url, run_sequence_filehandle)
+                with open(run_sequence_filename, 'wb+') as run_sequence_filehandle:
+                    fetch(run_url, run_sequence_filehandle, md5_checksum=run['fastq_md5'])
 
                 run_sequence_filenames.append(run_sequence_filename)
 
