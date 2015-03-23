dccc51cb3a2dce28a24d71c334aad34cf580fba8
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-03-23 20:11:41 +0000

    removed the old bowtie alignment task. good riddance

--- genome_alignment.py
+++ genome_alignment.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from multiprocessing import cpu_count
 import luigi
 import pybedtools
 from chromosomes import Chromosomes
@@ -10,168 +9,11 @@ from fastq_sequence import FastqSequence
 from genome_browser import GenomeSequence
 from genome_mappability import GenomeMappabilityTrack
 from task import Task, MetaTask
-from ena_downloader import ShortReadsForExperiment
 from genome_index import GenomeIndex
 import logging
 import os
-import tempfile
 import shutil
 from util import ensure_directory_exists_for_file
-
-
-class BowtieAlignmentTask(Task):
-
-    genome_version = GenomeIndex.genome_version
-    experiment_accession = ShortReadsForExperiment.experiment_accession
-    study_accession = ShortReadsForExperiment.study_accession
-
-    cell_type = ShortReadsForExperiment.cell_type
-    data_track = ShortReadsForExperiment.data_track
-
-    bowtie_seed = luigi.IntParameter(default=0)
-
-    pretrim_reads = luigi.BooleanParameter(default=False)
-
-    number_of_processes = luigi.IntParameter(default=int(cpu_count()/8), significant=False)
-
-    @property
-    def short_reads_task(self):
-        return ShortReadsForExperiment(experiment_accession=self.experiment_accession,
-                                        study_accession=self.study_accession,
-                                        cell_type=self.cell_type,
-                                        data_track=self.data_track)
-
-    def requires(self):
-        return [GenomeIndex(genome_version=self.genome_version),
-                self.short_reads_task]
-
-
-    @property
-    def parameters(self):
-        params = self.short_reads_task.parameters
-        params.append(self.genome_version)
-
-        if self.bowtie_seed != 0:
-            params.append(self.bowtie_seed)
-        if self.pretrim_reads:
-            params.append('trim')
-
-        return params
-
-
-    @property
-    def _extension(self):
-        return 'bam'
-
-    def output(self):
-        bam_output = super(BowtieAlignmentTask, self).output()
-
-        stdout_output = luigi.File(bam_output.path + '.stdout')
-
-        return bam_output, stdout_output
-
-    def run(self):
-        logger = self.logger()
-
-        from command_line_applications.archiving import unzip, tar
-        from command_line_applications.bowtie import bowtie2
-        from command_line_applications.samtools import samtools
-
-        bam_output, stdout_output = self.output()
-        stdout_output_abspath = os.path.abspath(stdout_output.path)
-        bam_output_abspath = os.path.abspath(bam_output.path)
-
-        index_output, sra_output = self.input()
-        index_abspath = os.path.abspath(index_output.path)
-        sra_output_abspath = os.path.abspath(sra_output.path)
-
-        logger.debug('Ensuring output directory exists')
-        # Make sure to create directories
-        try:
-            os.makedirs(os.path.dirname(stdout_output_abspath))
-        except OSError:
-            if not os.path.isdir(os.path.dirname(stdout_output_abspath)):
-                raise
-
-        try:
-            os.makedirs(os.path.dirname(bam_output_abspath))
-        except OSError:
-            if not os.path.isdir(os.path.dirname(bam_output_abspath)):
-                raise
-
-        sam_output_filename = 'alignments.sam'
-        bam_output_filename = 'alignments.bam'
-        stdout_filename = 'stats.txt'
-
-        current_working_directory = os.getcwdu()
-        temporary_directory = tempfile.mkdtemp(prefix='tmp-bt2align-')
-
-        try:
-            logger.debug('Changing directory to {}'.format(temporary_directory))
-            os.chdir(temporary_directory)
-
-            logger.debug('Unzipping index')
-            unzip(index_abspath)
-
-            logger.debug('Extracting sequences')
-            sequences_dirname = 'sequences'
-            os.makedirs(sequences_dirname)
-            tar('-xjf', sra_output_abspath, '--directory={}'.format(sequences_dirname))
-
-            sequences_to_align = os.listdir(sequences_dirname)
-
-            if self.pretrim_reads:
-
-                logger.debug('Pretrim sequences set, trimming')
-                from command_line_applications.sickle import sickle
-
-                trimmed_sequences_dirname = 'trimmed_sequences'
-                os.makedirs(trimmed_sequences_dirname)
-
-                trimmed_sequences = []
-                for sequence in sequences_to_align:
-                    assert sequence[-3:] == '.gz'
-                    sequence_without_gzip = sequence[:-3]
-
-                    logger.debug('Trimming {}'.format(sequence))
-                    sickle('se',
-                           '-f', os.path.join(sequences_dirname, sequence),
-                           '-t', 'sanger',
-                           '-o', os.path.join(trimmed_sequences_dirname, sequence_without_gzip))
-
-                    trimmed_sequences.append(sequence_without_gzip)
-
-                logger.debug('Deleting untrimmed sequences')
-                shutil.rmtree(sequences_dirname)
-
-                sequences_dirname = trimmed_sequences_dirname
-                sequences_to_align = trimmed_sequences
-
-            bowtie_align_string = ','.join([os.path.join(sequences_dirname, x) for x in sequences_to_align])
-            logging.debug('Bowtie string to align sequences: {!r}'.format(bowtie_align_string))
-
-            logging.debug('Running bowtie')
-            bowtie2('-q', '--very-sensitive',
-                    '-U', bowtie_align_string,
-                    '-x', self.genome_version,
-                    '--no-unal',
-                    '-p', self.number_of_processes,
-                    '--seed', self.bowtie_seed,
-                    '-S', sam_output_filename,
-                    _err=stdout_filename
-                    )
-
-            logging.debug('Converting SAM to BAM')
-            samtools('view', '-b', sam_output_filename, _out=bam_output_filename)
-
-            logging.debug('Moving files to correct locations')
-            shutil.move(stdout_filename, stdout_output_abspath)
-            shutil.move(bam_output_filename, bam_output_abspath)
-            logging.debug('Done')
-        finally:
-            logger.debug('Cleaning up')
-            os.chdir(current_working_directory)
-            shutil.rmtree(temporary_directory)
 
 class AlignedReadsBase(Task):
     genome_version = GenomeIndex.genome_version
