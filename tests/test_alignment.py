a6f9dbb7139b2416ecbe19915691628ec4975641
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-03-26 17:26:40 +0000

    forgot to add tests file

---
+++ tests/test_alignment.py
@@ -0,0 +1,193 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import unittest
+
+import pybedtools
+
+from chipalign.genome.genome_alignment import _remove_duplicates_from_bed, _resize_reads
+
+
+class TestFiltering(unittest.TestCase):
+    def sample_data(self):
+        data = [
+            ('chr1', '10', '30', 'duplicate-a1', '99', '+'),
+            ('chr1', '18', '68', 'neg-strand-duplicate-a1', '99', '-'),
+            ('chr1', '10', '40', 'duplicate-a2', '99', '+'),
+            ('chr1', '10', '35', 'non-duplicate-to-a', '99', '-'),
+            ('chr2', '10', '30', 'non-duplicate-to-a-as-chr2', '99', '+'),
+            ('chr1', '16', '68', 'neg-strand-duplicate-a2', '99', '-'),
+            ('chr1', '50', '80', 'non-duplicate-to-a-as different loc', '99', '+'),
+            ('chr1', '50', '80', 'non-duplicate-to-b-as different loc-neg', '99', '-'),
+        ]
+        input_ = pybedtools.BedTool(data)
+
+        expected_output = [
+            ('chr1', '10', '35', 'non-duplicate-to-a', '99', '-'),
+            ('chr2', '10', '30', 'non-duplicate-to-a-as-chr2', '99', '+'),
+            ('chr1', '50', '80', 'non-duplicate-to-a-as different loc', '99', '+'),
+            ('chr1', '50', '80', 'non-duplicate-to-b-as different loc-neg', '99', '-'),
+        ]
+        output_ = pybedtools.BedTool(expected_output)
+        return input_, output_
+
+
+    def test_duplicate_filtering_works(self):
+        input_, expected_output = self.sample_data()
+
+        output_ = _remove_duplicates_from_bed(input_)
+
+        self.assertEqual(expected_output, output_)
+
+
+class TestResizing(unittest.TestCase):
+
+
+    def test_resizing_produces_correct_length(self):
+
+        data = [
+            # Need to shorten
+            ('chr1', '10', '30', 'a', '99', '+'),  # Length: 20
+            ('chr1', '18', '68', 'b', '99', '-'),  # Length: 50
+            ('chr1', '10', '40', 'c', '99', '+'),  # Length: 30
+            ('chr2', '50', '80', 'h', '99', '-'),  # Length: 30
+            # Need to extend
+            ('chr1', '10', '15', 'e', '99', '+'),  # Length: 5
+            ('chr2', '50', '60', 'f', '99', '-')   # Length: 10
+        ]
+        input_ = pybedtools.BedTool(data)
+
+        # Large enough so we do not need to worry about boundaries
+        chromsizes = {'chr1': (0, 300), 'chr2': (0, 300)}
+
+        new_length = 20
+
+        actual_output = _resize_reads(input_, new_length=new_length,
+                                      chromsizes=chromsizes,
+                                      can_shorten=True,
+                                      can_extend=True)
+
+        lengths = map(lambda x: x.length, actual_output)
+        lengths_equal_to_truncation_length = [l == new_length for l in lengths]
+        self.assertTrue(all(lengths_equal_to_truncation_length), repr(lengths))
+
+    def test_resizing_works_on_correct_strand(self):
+
+        data = [
+            # Need to shorten
+            ('chr1', '10', '30', 'a', '99', '+'),  # Length: 20
+            ('chr1', '18', '68', 'b', '99', '-'),  # Length: 50
+            ('chr1', '10', '40', 'c', '99', '+'),  # Length: 30
+            ('chr2', '50', '80', 'h', '99', '-'),  # Length: 30
+            # Need to extend
+            ('chr1', '10', '15', 'e', '99', '+'),  # Length: 5
+            ('chr2', '50', '60', 'f', '99', '-')   # Length: 10
+        ]
+
+        new_length = 20
+
+        new_data = [
+            # Need to shorten
+            ('chr1', '10', '30', 'a', '99', '+'),  # Length: 20
+            ('chr1', '48', '68', 'b', '99', '-'),  # Length: 20
+            ('chr1', '10', '30', 'c', '99', '+'),  # Length: 20
+            ('chr2', '60', '80', 'h', '99', '-'),  # Length: 20
+            # Need to extend
+            ('chr1', '10', '30', 'e', '99', '+'),  # Length: 5
+            ('chr2', '40', '60', 'f', '99', '-')   # Length: 10
+        ]
+
+        input_ = pybedtools.BedTool(data)
+        expected_output = pybedtools.BedTool(new_data)
+
+        # Large enough so we do not need to worry about boundaries
+        chromsizes = {'chr1': (0, 300), 'chr2': (0, 300)}
+
+        actual_output = _resize_reads(input_, new_length=new_length,
+                                      chromsizes=chromsizes,
+                                      can_shorten=True,
+                                      can_extend=True)
+
+        self.assertEqual(expected_output, actual_output,
+                         msg="Expected:\n{}\nActual:\n{}".format(expected_output, actual_output))
+
+
+    def test_resizing_raises_exception_if_needed(self):
+        shorten_data = [
+            # Need to shorten
+            ('chr1', '18', '68', 'b', '99', '-'),  # Length: 50
+            ('chr1', '10', '40', 'c', '99', '+'),  # Length: 30
+            ('chr2', '50', '80', 'h', '99', '-'),  # Length: 30
+        ]
+        extend_data = [
+            # Need to extend
+            ('chr1', '10', '15', 'e', '99', '+'),  # Length: 5
+            ('chr2', '50', '60', 'f', '99', '-')  # Length: 10
+        ]
+
+        new_length = 20
+
+        # Large enough so we do not need to worry about boundaries
+        chromsizes = {'chr1': (0, 300), 'chr2': (0, 300)}
+
+        try:
+            _resize_reads(pybedtools.BedTool(shorten_data),
+                          new_length, chromsizes=chromsizes,
+                          can_shorten=True,
+                          can_extend=False)
+        except Exception as e:
+            self.fail('Unexpected exception {!r}'.format(e))
+
+        self.assertRaises(Exception,
+                          _resize_reads,
+                          pybedtools.BedTool(shorten_data + [extend_data[0]]),
+                          new_length, chromsizes=chromsizes,
+                          can_shorten=True,
+                          can_extend=False
+                          )
+
+        try:
+            _resize_reads(pybedtools.BedTool(extend_data),
+                          new_length, chromsizes=chromsizes,
+                          can_shorten=False,
+                          can_extend=True)
+        except Exception as e:
+            self.fail('Unexpected exception {!r}'.format(e))
+
+        self.assertRaises(Exception,
+                          _resize_reads,
+                          pybedtools.BedTool(extend_data + [shorten_data[0]]),
+                          new_length, chromsizes=chromsizes,
+                          can_shorten=False,
+                          can_extend=True
+                          )
+
+    def test_resizing_respects_chromosome_bounds(self):
+
+        data = [
+            # Need to extend
+            ('chr1', '0', '30', 'a', '99', '+'),  # Length: 30
+            ('chr2', '5', '25', 'h', '99', '-'),  # Length: 20
+        ]
+
+        new_length = 50
+
+        new_data = [
+           ('chr1', '0', '35', 'a', '99', '+'),  # Length: 35
+           ('chr2', '2', '25', 'h', '99', '-'),  # Length: 23
+        ]
+
+        # Specific, and rather unusual boundaries
+        chromsizes = {'chr1': (0, 35), 'chr2': (2, 300)}
+
+        input_ = pybedtools.BedTool(data)
+        expected_output = pybedtools.BedTool(new_data)
+
+        actual_output = _resize_reads(input_, new_length=new_length,
+                                      chromsizes=chromsizes,
+                                      can_shorten=True,
+                                      can_extend=True)
+
+        self.assertEqual(expected_output, actual_output,
+                         msg="Expected:\n{}\nActual:\n{}".format(expected_output, actual_output))
---
+++ tests/test_profile_base.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+import pybedtools
+from chipalign.profile.signal import weighted_means_from_intersection
+
+
+class TestGenomeWindows(unittest.TestCase):
+    sample_data = [('chr1', '9000', '9200', 'chr1', '0', '9971', '0.19923'),
+                   ('chr1', '9200', '9400', 'chr1', '0', '9971', '0.19923'),
+                   ('chr1', '9400', '9600', 'chr1', '0', '9971', '0.19923'),
+                   ('chr1', '9600', '9800', 'chr1', '0', '9971', '0.19923'),
+                   ('chr1', '9800', '10000', 'chr1', '0', '9971', '0.19923'),
+                   ('chr1', '9800', '10000', 'chr1', '9971', '10048', '0.17932'),
+                   ('chr1', '10000', '10200', 'chr1', '9971', '10048', '0.17932'),
+                   ('chr1', '10000', '10200', 'chr1', '10050', '10150', '0.4321'),
+                   ('chr1', '10200', '10400', '.', '-1', '-1', '.'),
+                   ('chr1', '10400', '10600', 'chr1', '10450', '10550', '0.02194'),
+                   ('chr2', '10400', '10600', 'chr2', '10550', '10600', '0.01234'),
+                   ]
+
+    def expected_output_factory(self, null):
+        return [('chr1', 9000, 9200, 0.19923),
+
+                ('chr1', 9200, 9400, 0.19923),
+                ('chr1', 9400, 9600, 0.19923),
+                ('chr1', 9600, 9800, 0.19923),
+                ('chr1', 9800, 10000,
+                 (0.19923 * (9971 - 9800) + 0.17932 * (10000 - 9971)) / 200.0),
+                ('chr1', 10000, 10200, ((10048 - 10000) * 0.17932
+                                        + (10150 - 10050) * 0.4321
+                                        + null * (200 - (10048 - 10000) - (10150 - 10050))
+                                        ) / 200.0),
+                ('chr1', 10200, 10400, null),
+                ('chr1', 10400, 10600, (
+                    (10550 - 10450) * 0.02194
+                    + (200 - (10550 - 10450)) * null
+                ) / 200.0),
+                ('chr2', 10400, 10600, (
+                    (10600 - 10550) * 0.01234
+                    + (200 - (10600 - 10550)) * null
+                ) / 200.0)]
+
+
+    def test_weighted_means_from_intersection_general_case(self):
+        null_value = 0
+        expected_output = self.expected_output_factory(null_value)
+        actual_output = weighted_means_from_intersection(pybedtools.BedTool(self.sample_data), 4, null_value=null_value)
+        actual_output = list(actual_output)
+
+        self.assertListEqual(expected_output, actual_output)
+
+    def test_weighted_means_from_intersection_nonzero_null(self):
+        null_value = 10000
+        expected_output = self.expected_output_factory(null_value)
+        actual_output = weighted_means_from_intersection(pybedtools.BedTool(self.sample_data), 4, null_value=null_value)
+        actual_output = list(actual_output)
+
+        self.assertListEqual(expected_output, actual_output)
+
+    def test_different_column_selection_is_fine(self):
+        sample_data = self.sample_data
+        column_shifted_sample_data = []
+        for chr_a, start_a, end_a, chr_b, start_b, end_b, value in sample_data:
+            column_shifted_sample_data.append((chr_a, start_a, end_a, chr_b, start_b, end_b, 1, value))
+
+
+        null_value = 0
+        expected_output = self.expected_output_factory(null_value)
+        actual_output = weighted_means_from_intersection(pybedtools.BedTool(column_shifted_sample_data), 5,
+                                                         null_value=null_value)
+        actual_output = list(actual_output)
+
+        self.assertListEqual(expected_output, actual_output)
+
+    def test_mean_function_can_be_passed(self):
+        null_value = 0
+        expected_output = self.expected_output_factory(null_value)
+        expected_output = [(x[0], x[1], x[2], -5) for x in expected_output]
+
+        actual_output = weighted_means_from_intersection(pybedtools.BedTool(self.sample_data), 4, null_value=null_value,
+                                                         mean_function=lambda __: -5)
+        actual_output = list(actual_output)
+
+        self.assertListEqual(expected_output, actual_output)
