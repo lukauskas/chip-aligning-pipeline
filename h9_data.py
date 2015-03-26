909407f788c326fb1fc31b26eab7ae9b88e4cb9f
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-03-26 11:27:59 +0000

    changes to h9_data

--- h9_data.py
+++ h9_data.py
@@ -45,11 +45,17 @@ class BRD4Signal(luigi.Task):
                                            )
 
     def requires(self):
-        return Signal(input_task=self.downloaded_h9_input_task, treatment_task=self.downloaded_h9_input_task)
+        return Signal(input_task=self.brd4_consolidated_task, treatment_task=self.downloaded_h9_input_task)
+
+    def output(self):
+        return self.requires().output()
 
     def complete(self):
         return self.requires().complete()
 
+    @property
+    def parameters(self):
+        return [self.cell_type, self.genome_version, self.aligner, self.max_sequencing_depth, self.chromosomes]
 
 class BRD4Alignments(luigi.Task):
     genome_version = luigi.Parameter()
@@ -60,7 +66,7 @@ class BRD4Alignments(luigi.Task):
 
 
     def requires(self):
-        brd4_signal = ['SRR1537736', 'SRR1537737'] # GSM1466835 BRD4 Vehicle- treated 6h
+        brd4_signal = ['SRR1537736', 'SRR1537737']  # GSM1466835 BRD4 Vehicle- treated 6h
         brd4_control = ['SRR1537734']  # IgG GSM1466833
 
         return [ConsolidatedReads(genome_version=self.genome_version,
@@ -78,8 +84,8 @@ class BRD4Alignments(luigi.Task):
     def complete(self):
         return all(map(lambda x: x.complete(), self.requires()))
 
-class H3K56acRefData(luigi.Task):
 
+class H3K56acRefData(luigi.Task):
     genome_version = luigi.Parameter()
     aligner = luigi.Parameter(default='pash')
 
@@ -99,45 +105,8 @@ class H3K56acRefData(luigi.Task):
     def complete(self):
         return all(map(lambda x: x.complete(), self.requires()))
 
-class AllSignals(luigi.Task):
-
-    genome_version = luigi.Parameter(default='hg19')
-    chromosomes = luigi.Parameter(default='female')
-    cell_type = luigi.Parameter(default='E008')
-
-    ext_size = luigi.IntParameter(default=170)
-    window_size = luigi.IntParameter(default=200)
-    read_length = luigi.IntParameter(default=36)
-
-    def requires(self):
-        try:
-            return self.__requires_cache
-        except AttributeError:
-
-            downloadable_signals = DownloadableSignalTracks(genome_version=self.genome_version, cell_type=self.cell_type)
-            luigi.build([downloadable_signals])
-
-            mappable_windows = FullyMappableGenomicWindows(genome_version=self.genome_version,
-                                                           chromosomes=self.chromosomes,
-                                                           read_length=self.read_length,
-                                                           ext_size=self.ext_size,
-                                                           window_size=self.window_size)
-
-            tracks = downloadable_signals.output().load()
-            signals = [DownloadedSignal(genome_version=self.genome_version, cell_type=self.cell_type,
-                                         chromosomes=self.chromosomes, track=track) for track in tracks]
-
-
-            requires = [BinnedSignal(bins_task=mappable_windows, signal_task=signal) for signal in signals]
-            self.__requires_cache = requires
-
-            return self.__requires_cache
-
-    def complete(self):
-        return all([r.complete() for r in self.requires()])
 
 class SignalDataFrame(Task):
-
     genome_version = luigi.Parameter(default='hg19')
     chromosomes = luigi.Parameter(default='female')
     cell_type = luigi.Parameter(default='E008')
@@ -146,33 +115,58 @@ class SignalDataFrame(Task):
     window_size = luigi.IntParameter(default=200)
     read_length = luigi.IntParameter(default=36)
 
-    def requires(self):
+    aligner = BRD4Signal.aligner
+
+    max_sequencing_depth = BRD4Signal.max_sequencing_depth
+
+    def _possible_signal_tracks(self):
         try:
-            return self.__requires_cache
+            return self.__signals_cache
         except AttributeError:
-            downloadable_signals = DownloadableSignalTracks(genome_version=self.genome_version, cell_type=self.cell_type)
+            downloadable_signals = DownloadableSignalTracks(genome_version=self.genome_version,
+                                                            cell_type=self.cell_type)
             luigi.build([downloadable_signals])
 
-            mappable_windows = FullyMappableGenomicWindows(genome_version=self.genome_version,
-                                                           chromosomes=self.chromosomes,
-                                                           read_length=self.read_length,
-                                                           ext_size=self.ext_size,
-                                                           window_size=self.window_size)
-
             tracks = downloadable_signals.output().load()
+            signals = {track: DownloadedSignal(genome_version=self.genome_version, cell_type=self.cell_type,
+                                               chromosomes=self.chromosomes, track=track) for track in tracks}
 
-            signals = [DownloadedSignal(genome_version=self.genome_version, cell_type=self.cell_type,
-                                         chromosomes=self.chromosomes, track=track) for track in tracks]
+            brd4_signal = BRD4Signal(genome_version=self.genome_version,
+                                     aligner=self.aligner,
+                                     chromosomes=self.chromosomes,
+                                     max_sequencing_depth=self.max_sequencing_depth,
+                                     )
+
+            signals['BRD4'] = brd4_signal
+
+            self.__signals_cache = signals
+
+            return self.__signals_cache
 
 
-            requires = [BinnedSignal(bins_task=mappable_windows, signal_task=signal) for signal in signals]
-            self.__requires_cache = requires
+    def _binned_signal_tasks(self):
+        signals = self._possible_signal_tracks()
 
-            return self.__requires_cache
+        mappable_windows = FullyMappableGenomicWindows(genome_version=self.genome_version,
+                                                       chromosomes=self.chromosomes,
+                                                       read_length=self.read_length,
+                                                       ext_size=self.ext_size,
+                                                       window_size=self.window_size)
+
+        binned_signals = {track: BinnedSignal(bins_task=mappable_windows, signal_task=signal)
+                          for track, signal in signals.iteritems()}
+
+        return binned_signals
+
+
+    def requires(self):
+        return self._binned_signal_tasks().values()
+
 
     @property
     def _extension(self):
         return 'df'
+
 
     @property
     def parameters(self):
@@ -180,15 +174,18 @@ class SignalDataFrame(Task):
                 'w{}'.format(self.window_size), 'e{}'.format(self.ext_size),
                 'r{}'.format(self.read_length), self.chromosomes]
 
+
     def output(self):
         path = super(SignalDataFrame, self).output().path
 
         return DataFrameFile(path)
+
+
     def run(self):
         logger = self.logger()
 
         series = []
-        for task in self.requires():
+        for track, task in self._binned_signal_tasks():
             input_ = task.output()
             logger.info('Reading {}'.format(input_.path))
             input_series = pd.read_table(input_.path,
@@ -196,7 +193,7 @@ class SignalDataFrame(Task):
                                          index_col=['chromosome', 'start', 'end'], compression='gzip')
 
             input_series = input_series['p_value']
-            input_series.name = task.signal_task.track
+            input_series.name = track
 
             series.append(input_series)
 
@@ -204,6 +201,31 @@ class SignalDataFrame(Task):
         df = pd.concat(series, axis=1)
         logger.info('Outputting')
         self.output().dump(df)
+
+
+class SignalTest(Task):
+    genome_version = luigi.Parameter(default='hg19')
+    cell_type = luigi.Parameter(default='E008')
+    track = luigi.Parameter(default='H3K56ac')
+    chromosomes = luigi.Parameter(default='female')
+
+    def requires(self):
+        input_reads = DownloadedConsolidatedReads(genome_version=self.genome_version,
+                                                  cell_type=self.cell_type,
+                                                  track='Input',
+                                                  chromosomes=self.chromosomes
+                                                  )
+
+        track_reads = DownloadedConsolidatedReads(genome_version=self.genome_version,
+                                                  cell_type=self.cell_type,
+                                                  track=self.track,
+                                                  chromosomes=self.chromosomes
+                                                  )
+
+        return Signal(input_task=input_reads, treatment_task=track_reads)
+
+    def complete(self):
+        return self.requires().complete()
 
 
 if __name__ == '__main__':
