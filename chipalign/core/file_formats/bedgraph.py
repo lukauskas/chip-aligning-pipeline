33c95f79779f3dd9ed7a264b776def0b70336c11
Saulius Lukauskas <luksaulius@gmail.com>
2015-04-04 17:04:50 +0100

    fixed bedgraph format

--- chipalign/core/file_formats/bedgraph.py
+++ chipalign/core/file_formats/bedgraph.py
@@ -3,9 +3,19 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import luigi
+import luigi.format
+
 import pandas as pd
 
 class BedGraph(luigi.File):
+    def __init__(self, path=None, **kwargs):
+        if path.endswith('gz'):
+            format_ = kwargs.get('format', luigi.format.Gzip)
+        else:
+            format_ = kwargs.get('format', None)
+
+        super(BedGraph, self).__init__(path=path, format=format_, **kwargs)
+
     def to_pandas_series(self):
         series = pd.read_table(self.path,
                                header=0, names=['chromosome', 'start', 'end', 'value'],
