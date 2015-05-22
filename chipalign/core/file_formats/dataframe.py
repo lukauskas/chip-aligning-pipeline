aca285bcfcbe823bf14032c0d47e93027a6b9961
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-05-22 16:00:57 +0100

    dataframe now being stored as hdf

--- chipalign/core/file_formats/dataframe.py
+++ chipalign/core/file_formats/dataframe.py
@@ -7,8 +7,12 @@ import numpy as np
 import tempfile
 import shutil
 import pandas as pd
+from chipalign.core.util import temporary_file
+
 
 class DataFrameFile(object):
+
+    __HDF_KEY = 'table'
 
     def __init__(self, path):
         self._path = path
@@ -20,6 +24,12 @@ class DataFrameFile(object):
     def exists(self):
         return os.path.exists(self.path)
 
+    def __dump_hdf(self, df, hdf_filename):
+        df.to_hdf(hdf_filename, self.__HDF_KEY)
+
+    def __load_hdf(self, hdf_filename):
+        return pd.read_hdf(hdf_filename, self.__HDF_KEY)
+
     def dump(self, df, verify=True):
         self_path = self.path
         try:
@@ -28,56 +38,13 @@ class DataFrameFile(object):
             if not os.path.isdir(os.path.dirname(self_path)):
                 raise
 
-        __, tmp_file = tempfile.mkstemp(prefix=self.path, suffix='.npz', dir='.')
-        try:
-            payload = {'columns': np.array(df.columns),
-                       'index': np.array(df.index),
-                       'values': df.values,
-                       'dtypes': np.array(df.dtypes),
-                       'index_names': np.array(df.index.names)}
-            if isinstance(df.index, pd.MultiIndex):
-                payload['multi_index'] = True
-
-            np.savez(tmp_file, **payload)
-
+        with temporary_file() as temporary_filename:
+            self.__dump_hdf(df, temporary_filename)
             if verify:
-                loaded_payload = np.load(tmp_file)
-                if len(loaded_payload.keys()) != len(payload.keys()):
-                    raise IOError('Something went wrong writing the payload')
-                else:
-                    for key, value in payload.iteritems():
-
-                        if isinstance(value, np.ndarray):
-                            not_equal = not np.array_equal(value, loaded_payload[key])
-                        else:
-                            not_equal = value != loaded_payload[key]
-
-                        if not_equal:
-                            raise IOError('Something went wrong when writing the payload. Keys {} don\'t match'.format(key))
-
-            shutil.move(tmp_file, self_path)
-        finally:
-            try:
-                os.unlink(tmp_file)
-            except OSError:
-                if os.path.isfile(tmp_file):
-                    raise
+                df2 = self.__load_hdf(temporary_filename)
+                if not df2.equals(df):
+                    raise Exception('DataFrame dump verification failed')
+            shutil.move(temporary_filename, self_path)
 
     def load(self):
-        data = np.load(self.path)
-        df = pd.DataFrame(data['values'], index=data['index'], columns=data['columns'])
-
-        for column, dtype in zip(data['columns'], data['dtypes']):
-            df[column] = df[column].astype(dtype)
-
-        try:
-            multi_index = data['multi_index']
-        except KeyError:
-            multi_index = None
-
-        if multi_index:
-            df.index = pd.MultiIndex.from_tuples(df.index, names=data['index_names'])
-        else:
-            df.index.names = data['index_names']
-
-        return df
+        return self.__load_hdf(self.path)
--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['luigi', 'numpy', 'pandas', 'sh', 'pyyaml', 'pybedtools', 'requests'],
+    install_requires=['luigi', 'numpy', 'pandas', 'sh', 'pyyaml', 'pybedtools', 'requests', 'tables'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
