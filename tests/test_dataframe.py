657dea6b163291e5f27a956c47b1eb5467d19e07
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-05-22 17:36:02 +0100

    hdf allows saving of series objects too

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -10,6 +10,7 @@ import tempfile
 import pandas as pd
 
 from chipalign.core.file_formats.dataframe import DataFrameFile
+from chipalign.core.util import temporary_file
 
 
 class TestDataFrameSaving(unittest.TestCase):
@@ -72,3 +73,49 @@ class TestDataFrameSaving(unittest.TestCase):
             except OSError:
                 if os.path.isfile(tmp_filename):
                     raise
+
+class TestSeriesSaving(unittest.TestCase):
+
+    def test_sample_series(self):
+        sample_series = pd.Series([1, 2 ,3, 4, 5])
+
+        with temporary_file(cleanup_on_exception=True) as tmp_filename:
+            series_file = DataFrameFile(tmp_filename)
+            series_file.dump(sample_series)
+
+            roundtrip_series = series_file.load()
+            self.assertTrue(sample_series.equals(roundtrip_series),
+                            msg="Expected:\n{0}\n{0._data}\n---\nGot:\n{1}\n{1._data}\n".format(sample_series,
+                                                                                                roundtrip_series))
+            self.assertEqual(sample_series.name, roundtrip_series.name)
+
+    def test_multiindex_df(self):
+        index = pd.MultiIndex.from_tuples([('a', 1), ('b', 2), ('c', -1), ('d', 5)], names=['letter', 'number'])
+        sample_series = pd.Series([1, 2, 3, 4], index=index)
+
+        with temporary_file(cleanup_on_exception=True) as tmp_filename:
+            series_file = DataFrameFile(tmp_filename)
+            series_file.dump(sample_series)
+
+            roundtrip_series = series_file.load()
+            self.assertTrue(sample_series.equals(roundtrip_series),
+                            msg="Expected:\n{0}\n{0._data}\n---\nGot:\n{1}\n{1._data}\n".format(sample_series,
+                                                                                                roundtrip_series))
+            self.assertEqual(sample_series.name, roundtrip_series.name)
+
+
+    def test_named_index_df(self):
+
+        index = pd.Index([4, 3, 2, 1], name='derp')
+        sample_series = pd.Series([1, 2, 3, 4], index=index)
+
+        with temporary_file(cleanup_on_exception=True) as tmp_filename:
+            series_file = DataFrameFile(tmp_filename)
+            series_file.dump(sample_series)
+
+            roundtrip_series = series_file.load()
+            self.assertTrue(sample_series.equals(roundtrip_series),
+                            msg="Expected:\n{0}\n{0._data}\n---\nGot:\n{1}\n{1._data}\n".format(sample_series,
+                                                                                                roundtrip_series))
+            self.assertEqual(sample_series.name, roundtrip_series.name)
+            self.assertEqual(roundtrip_series.index.name, sample_series.index.name)
