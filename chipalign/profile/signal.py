44b1ce9ca82cec9174612e8d05a152c1c09a6e9b
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-05-25 14:34:05 +0100

    everything should now use signalseries

--- chipalign/profile/signal.py
+++ chipalign/profile/signal.py
@@ -9,6 +9,7 @@ import luigi
 import pybedtools
 import numpy as np
 from chipalign.core.file_formats.bedgraph import BedGraph
+from chipalign.core.file_formats.dataframe import DataFrameFile
 
 from chipalign.core.task import Task
 
@@ -196,6 +197,27 @@ class BinnedSignal(Task):
 
         with self.output().open('w') as f:
             self.compute_profile(bins_task_abspath, signal_task_abspath, f)
+
+
+class SignalSeries(Task):
+
+    bedgraph_task = luigi.Parameter()
+
+    def requires(self):
+        assert isinstance(self.bedgraph_task.output(), BedGraph)
+        return self.bedgraph_task
+
+    @property
+    def _output_class(self):
+        return DataFrameFile
+
+    @property
+    def _extension(self):
+        return '.series'
+
+    def run(self):
+        series = self.input().to_pandas_series()
+        self.output().dump(series)
 
 if __name__ == '__main__':
     from chipalign.genome.genome_mappability import FullyMappableGenomicWindows
