4dba22309aee5ba6dc563f42c4ddd108aaba1f63
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-05-29 18:41:47 +0100

    refactored downloaded signal a bit

--- chipalign/roadmap_data/downloaded_signal.py
+++ chipalign/roadmap_data/downloaded_signal.py
@@ -3,16 +3,15 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import os
-import re
 import shutil
 
-import requests
-from chipalign.core.file_formats.bedgraph import BedGraph
+import luigi
 
+from chipalign.core.file_formats.bedgraph import BedGraph
 from chipalign.core.task import Task
 from chipalign.core.downloader import fetch
-from chipalign.core.file_formats.yaml_file import YamlFile
-import luigi
+from chipalign.roadmap_data.signal_tracks_list import SignalTracksList
+
 
 class DownloadedSignal(Task):
 
@@ -28,12 +27,8 @@ class DownloadedSignal(Task):
         return self.downloadable_signal_task.output().load()[self.track]
 
     @property
-    def parameters(self):
-        return [self.cell_type, self.track, self.genome_version, self.chromosomes]
-
-    @property
     def downloadable_signal_task(self):
-        return DownloadableSignalTracks(genome_version=self.genome_version, cell_type=self.cell_type)
+        return SignalTracksList(genome_version=self.genome_version, cell_type=self.cell_type)
 
     def requires(self):
         return [self.chromosomes_task, self.downloadable_signal_task]
@@ -83,48 +78,3 @@ class DownloadedSignal(Task):
             logger.info('Moving')
             shutil.move(filtered_and_sorted, output_abspath)
 
-class DownloadableSignalTracks(Task):
-
-    cell_type = luigi.Parameter()
-    genome_version = luigi.Parameter()
-
-    @property
-    def parameters(self):
-        return [self.cell_type, self.genome_version]
-
-    def url(self):
-        if self.genome_version == 'hg19':
-            return 'http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/'
-        else:
-            raise ValueError('Unsupported genome version {!r}'.format(self.genome_version))
-
-    def output(self):
-        return YamlFile(super(DownloadableSignalTracks, self).output().path)
-
-    @property
-    def _extension(self):
-        return 'yml'
-
-    def run(self):
-        url = self.url()
-        response = requests.get(url)
-        response.raise_for_status()
-
-        hrefs = re.findall('href="([^"]+)"', response.text)
-        bigwigs = filter(lambda x: x.endswith('bigwig'), hrefs)
-
-        urls_for_cell_type = {}
-
-        for filename in bigwigs:
-            match = re.match('(?P<cell_type>\w+)-(?P<track>.*?).pval.signal.bigwig', filename)
-            if match is None:
-                raise Exception('Could not parse {!r}'.format(filename))
-
-            if match.group('cell_type') == self.cell_type:
-                full_url = os.path.join(url, filename)
-                urls_for_cell_type[match.group('track')] = full_url
-
-        if not urls_for_cell_type:
-            raise Exception('No URLs for cell type {!r} have been recovered'.format(self.cell_type))
-
-        self.output().dump(urls_for_cell_type)
---
+++ chipalign/roadmap_data/signal_tracks_list.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import os
+import re
+import luigi
+import requests
+from chipalign.core.file_formats.yaml_file import YamlFile
+from chipalign.core.task import Task
+
+class SignalTracksList(Task):
+
+    cell_type = luigi.Parameter()
+    genome_version = luigi.Parameter()
+
+    @property
+    def parameters(self):
+        return [self.cell_type, self.genome_version]
+
+    def url(self):
+        if self.genome_version == 'hg19':
+            return 'http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/'
+        else:
+            raise ValueError('Unsupported genome version {!r}'.format(self.genome_version))
+
+    def output(self):
+        return YamlFile(super(SignalTracksList, self).output().path)
+
+    @property
+    def _extension(self):
+        return 'yml'
+
+    def run(self):
+        url = self.url()
+        response = requests.get(url)
+        response.raise_for_status()
+
+        hrefs = re.findall('href="([^"]+)"', response.text)
+        bigwigs = filter(lambda x: x.endswith('bigwig'), hrefs)
+
+        urls_for_cell_type = {}
+
+        for filename in bigwigs:
+            match = re.match('(?P<cell_type>\w+)-(?P<track>.*?).pval.signal.bigwig', filename)
+            if match is None:
+                raise Exception('Could not parse {!r}'.format(filename))
+
+            if match.group('cell_type') == self.cell_type:
+                full_url = os.path.join(url, filename)
+                urls_for_cell_type[match.group('track')] = full_url
+
+        if not urls_for_cell_type:
+            raise Exception('No URLs for cell type {!r} have been recovered'.format(self.cell_type))
+
+        self.output().dump(urls_for_cell_type)
