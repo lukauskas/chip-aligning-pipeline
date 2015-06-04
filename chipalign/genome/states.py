a89b9f32a29a045249eb471a4a594160166ed9c0
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2015-06-04 17:43:05 +0100

    added pallete for chromatin states

--- chipalign/genome/states.py
+++ chipalign/genome/states.py
@@ -49,6 +49,44 @@ class ChromatinStates(Task):
 
         return url_format.format(cell_type=self.cell_type)
 
+    def color_palette(self):
+
+        if self.number_of_states == 18:
+            import palettable
+            unique_state_cmap = palettable.tableau.Tableau_20
+
+            return {
+                # Red-ish colours for TSS
+                '1_TssA': unique_state_cmap.hex_colors[6],
+                '2_TssFlnk': unique_state_cmap.hex_colors[7],
+                '3_TssFlnkU': unique_state_cmap.hex_colors[12],
+                '4_TssFlnkD': unique_state_cmap.hex_colors[13],
+                # Green-ish colors for transcribed regions
+                '5_Tx': unique_state_cmap.hex_colors[4],
+                '6_TxWk': unique_state_cmap.hex_colors[5],
+                # Yellow-ish colors for genic enhancers
+                '7_EnhG1': unique_state_cmap.hex_colors[16],
+                '8_EnhG2': unique_state_cmap.hex_colors[17],
+                # Orange-ish colors for active enhancers
+                '9_EnhA1': unique_state_cmap.hex_colors[2],
+                '10_EnhA2': unique_state_cmap.hex_colors[3],
+                # Ran out of yellow-ish colours, so brown-ish it is
+                '11_EnhWk': unique_state_cmap.hex_colors[11],
+                # Purple for repeats
+                '12_ZNF/Rpts': unique_state_cmap.hex_colors[8],
+                # Brown for heterochromatin
+                '13_Het': unique_state_cmap.hex_colors[10],
+                # Light purple for bivalent tss
+                '14_TssBiv': unique_state_cmap.hex_colors[9],
+                # Blue for bivalent
+                '15_EnhBiv': unique_state_cmap.hex_colors[0],
+                '16_ReprPC': unique_state_cmap.hex_colors[14],
+                '17_ReprPCWk': unique_state_cmap.hex_colors[15],
+                '18_Quies': unique_state_cmap.hex_colors[1],
+            }
+        else:
+            raise NotImplementedError
+
     def run(self):
 
         __, tmp_download_file = tempfile.mkstemp()
