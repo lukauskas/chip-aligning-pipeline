61311420ec2e98fb968c7fb20c50d9e2e3dc746a
Saulius Lukauskas <saulius.lukauskas13@imperial.ac.uk>
2016-06-20 13:26:55 +0100

    added tasks to download filtered reads from roadmap, as well as metadata parsing utilities

---
+++ chipalign/database/roadmap/downloaded_filtered_reads.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import gzip
+import luigi
+from chipalign.core.downloader import fetch
+from chipalign.core.task import Task
+from chipalign.core.util import temporary_file
+
+import os
+
+class RoadmapDownloadedFilteredReads(Task):
+    """
+    Downloads filtered reads from `ROADMAP`_. This task is designed to be a
+    drop-in replacement for :class:`~chipalign.alignment.filtering.FilteredReads` task.
+
+    :param uri: uri of the read to download, as reported by
+    :func:`~chipalign.database.roadmap.settings.downloadable_unconsolidated_reads`
+
+    .. _ROADMAP: http://egg2.wustl.edu/roadmap/web_portal/processed_data.html#ChipSeq_DNaseSeq
+
+    .. seealso:: :func:`~chipalign.database.roadmap.settings.downloadable_unconsolidated_reads`
+    """
+
+    uri = luigi.Parameter()  # URI of the read to download
+
+    @property
+    def task_class_friendly_name(self):
+        return 'RDFR'
+
+    @property
+    def parameters(self):
+        # Just return the basename
+        basename = os.path.basename(self.uri).replace('.filt.tagAlign.gz', '')
+        return [basename]
+
+    @property
+    def _extension(self):
+        return 'tagAlign.gz'
+
+    def run(self):
+        logger = self.logger()
+        url = self.url()
+
+        self.ensure_output_directory_exists()
+
+        with temporary_file() as tf:
+            logger.info('Fetching: {} to {}'.format(url, tf))
+            with open(tf, 'w') as f:
+                fetch(url, f)
+
+            # Essentially this re-gzips the downloaded reads output
+            # without this, pybedtools fails to read it for whatever reason
+            # see the appropriate test
+            logger.info('Saving output')
+            with gzip.GzipFile(tf) as in_:
+                with self.output().open('w') as out_:
+                    out_.writelines(in_)
--- chipalign/database/roadmap/settings.py
+++ chipalign/database/roadmap/settings.py
@@ -1,5 +1,12 @@
+import logging
 import os
+
+import fnmatch
+import klepto
 import pandas as pd
+from functools32 import lru_cache
+import requests
+import re
 
 _CONSOLIDATED_METATADATA_FILE = os.path.join(os.path.dirname(__file__), '_qc_metadata/consolidated_epigenomes_qc.csv')
 
@@ -34,3 +41,111 @@ COMMON_CELL_TYPE_NAMES = {
     'E129': 'Osteoblast',
     'E017': 'IMR90',
 }
+
+
+@lru_cache(None)
+def consolidation_summary_metadata():
+    data = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                    '_qc_metadata/consolidated_summary.csv'), header=[0, 1])
+
+    # Remove first row as it is just a summary row of counts
+    data = data.iloc[1:]
+
+    # Set epigenome id as index
+    data = data.set_index('Epigenome ID (EID)')
+    # Remove the weird tupling that happens for this column
+    data.index = [i[0] for i in data.index]
+
+    return data
+
+
+def consolidated_filename_patterns(cell_type, target):
+    data = consolidation_summary_metadata()
+    filenames = data.loc[cell_type, ('Pool Filenames', target)]
+
+    if pd.isnull(filenames):
+        return []
+    else:
+        fnames = filenames.split(';')
+
+        clean_fnames = []
+        for fname in fnames:
+            # Not entirely sure why they give .bed.gz sometimes
+            if fname.endswith('.bed.gz'):
+                fname = fname.replace('.bed.gz', '.filt.tagAlign.gz')
+
+            clean_fnames.append(fname)
+
+        return clean_fnames
+
+
+@lru_cache(None)
+def consolidated_read_download_uris(cell_type, target):
+    patterns = consolidated_filename_patterns(cell_type, target)
+    if not patterns:
+        return []
+
+    all_uris = downloadable_unconsolidated_reads()
+    uris = []
+    for pattern in patterns:
+        for uri in all_uris:
+            basename = os.path.basename(uri)
+
+            if fnmatch.fnmatchcase(basename, pattern):
+                uris.append(uri)
+
+    if not uris:
+        logger = logging.getLogger('chipalign.database.roadmap.settings'
+                                   '.consolidated_read_download_uris')
+        logger.debug('Patterns found for cell_type={!r}, target={!r}:\n{!r}'.format(cell_type,
+                                                                                    target,
+                                                                                    patterns))
+
+        raise Exception('Patterns found, but no uris could be parsed for cell_type={!r}, '
+                        'target={!r}'.format(cell_type, target))
+    return uris
+
+
+@klepto.lru_cache()
+def downloadable_unconsolidated_reads():
+
+    root_uri = 'http://egg2.wustl.edu/roadmap/data/byFileType/alignments/unconsolidated/'
+
+    queue = []
+    queue.append(root_uri)
+
+    answers = []
+    while queue:
+        uri = queue.pop()
+        response = requests.get(uri)
+        response.raise_for_status()
+
+        text = response.text
+        hrefs = filter(lambda x: not x.startswith('?') and not x.startswith('/'),
+                       re.findall('<a\s+href="([^"]+)"', text))
+
+        for href in hrefs:
+            full_uri = ''.join([uri, href])
+            if href.endswith('/'):
+                queue.append(full_uri)
+            elif href.endswith('.tagAlign.gz'):
+                answers.append(full_uri)
+            else:
+                continue
+
+    return answers
+
+
+@lru_cache(100)
+def targets_for_cell_line(cell_line):
+
+    metadata = consolidation_summary_metadata()
+    tracks = metadata.loc[cell_line, 'Pool Filenames'].dropna().index
+
+    def is_histone(track):
+        return track != 'Input' and track != 'DNase' \
+               and track != 'RNA-seq' and track != 'RRBS' \
+               and track != 'WGBS' and track != 'mCRF'
+
+    tracks = filter(is_histone, tracks)
+    return tracks
